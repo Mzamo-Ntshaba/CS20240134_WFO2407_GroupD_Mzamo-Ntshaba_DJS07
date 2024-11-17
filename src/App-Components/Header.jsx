fcc25ad6fc5c51b65f430a4aac9c4e28f2ae8b74
Mzamo Ntshaba <mzamontshaba14@gmail.com>
2024-11-17 16:49:38 +0200

    Updated react header  component

---
+++ src/App-Components/Header.jsx
@@ -0,0 +1,15 @@
+
+//Headeder component
+// Uses classes from
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="header--image" src="./images/troll-face.png" alt="logo"/>
+      <h1 className="header--title">Meme Generator</h1>
+      <p className="header--project">React Course - Project 3</p>
+    </div>
+  )
+}
+
+export default Header;
