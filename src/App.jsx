87962e44a10beafd7b489a25a2effce3561c3cb3
Mzamo Ntshaba <mzamontshaba14@gmail.com>
2024-11-17 16:50:23 +0200

    Added config files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from './App-Components/Header.jsx'
+import Meme from './App-Components/Meme.jsx'
 
 function App() {
 
   return (
     <>
-     
+     <Header/>
+     <Meme />
     </>
   )
 }
